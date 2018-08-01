0e134b9d4da6244dd7e83b623422ed70750facb3
Dylan Duunk <0948392@hr.nl>
2018-08-01 23:13:25 +0200

    Added Starting Code + Added Latex Document Setup
    Added Starting Code for Arduino + Added Latex Setup for a neat concept document

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "main.h"
+
+void setup() {
+    Serial.begin(9600);
+    /** 
+     * TODO: 
+     * pinMode INPUT 
+     * INPUT equals the wire between the CDI and
+     * the pickup
+     * 
+     * TODO: pinMode OUPUT 
+     * 
+     */
+}
+
+void loop() {
+    /**
+     * TODO:
+     * if INPUT == 0 (or < defined number)
+     * send a signal that equals to 45km/h to 
+     * the pickup
+     * 
+     * else if INPUT > 0 (or > defined number)
+     * send the original signal to the pickup
+     */
+}
---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define DEBUG_MODE
+
+#include "Arduino.h"
+
+void setup();
+void loop();
+
+#endif //MAIN_H

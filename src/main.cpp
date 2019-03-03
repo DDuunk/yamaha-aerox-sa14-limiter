6fdd9992d0103db3eae81f1496bb020a5c358899
Dylan Duunk <0948392@hr.nl>
2019-03-03 19:08:24 +0100

    Some Tweaks to the code
    Some Tweaks to the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,46 @@
 #include "main.h" // Include our library
 
 void setup() {
-    /*
-     * Opens a Serial Port on 9600 baudrate (mostlty standard on Arduino) for debugging purposes
-     */
+    // Opens a Serial Port on 9600 baudrate for debugging purposes
     Serial.begin(9600);
 
-    /*
-     * PinMode Defines
-     */
-    pinMode(relayPin, OUTPUT);          // Relay is an output
-    pinMode(detectorPin, INPUT_PULLUP); // Sensor is an input with a pullup resistor
+    
+    // PinMode defines
+    pinMode(relayPin, OUTPUT);        // Relay is an output
+    pinMode(sensorPin, INPUT_PULLUP); // Sensor is an input with a pullup resistor
 }
 
 void loop() {
-    prevdetectorstate = digitalRead(detectorPin); // Sign prevdetectorstate to the value of detectorPin
-    beginTime = millis();                         // Start millis
+    // Sign prevdetectorstate to the value of sensorPin
+    prevdetectorstate = digitalRead(sensorPin);
+    beginTime = millis();             // Start millis
 
-    /*
-     * When the current millis - beginTime is less then 500ms
-     * - First sign detectorstate to the value of detectorPin
-     * - If prevdetectorstate is not equal to detectorstate AND detectorstate 
-     *   has the value of true: add a pulse
-     * - Sign prevdetectorstate to detectorstate to reset
-     */
+    // When the current millis - beginTime is less then 500ms
+    // - First sign detectorstate to the value of sensorPin
+    // - If prevdetectorstate is not equal to detectorstate AND detectorstate 
+    //   has the value of true: add a pulse
+    // - Sign prevdetectorstate to detectorstate to reset
     while ((millis() - beginTime) < 500) {
-        detectorstate = digitalRead(detectorPin);
+        detectorstate = digitalRead(sensorPin);
         if ((prevdetectorstate != detectorstate) && (detectorstate == true)) {
             pulse++;
         }
         prevdetectorstate = detectorstate;
     }
 
-    /*
-     * Print the value of pulse * 15 to the Serial port and convert the value to decimal
-     */
+    // Print the value of pulse * 15 to the Serial port and convert the value to decimal
     #ifdef DEBUG_MODE
         Serial.println((pulse * 15), DEC); 
     #endif
     
-    /*
-     * If the value of pulse * 15 is bigger or equal to 40: turn relay ON
-     * If the value of pulse * 15 is lower than 40 : turn relay off
-     * Because of the pullup resistor LOW == ON and HIGH == OFF!
-     */
+    // If the value of pulse * 15 is bigger or equal to 40: turn relay ON
+    // If the value of pulse * 15 is lower than 40 : turn relay off
+    // Because of the pullup resistor LOW == ON and HIGH == OFF!
     if ((pulse * 15) >= 40) {
         digitalWrite(relayPin, LOW);
     } else {
         digitalWrite(relayPin, HIGH);
     }
 
-    /*
-     * Reset the value of pulse
-     */ 
-    pulse = 0;
+    pulse = 0; // Reset the value of pulse
 }
